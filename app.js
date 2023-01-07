16b66af2b5c991fc114bb7bc29835884f6f68f72
yaoleksa <mryaremchyk@gmail.com>
2023-01-07 22:19:21 +0200

    improve logic

--- app.js
+++ app.js
@@ -11,25 +11,29 @@ app.use(express.static(publicPath));
 // handler function block
 
 const checkPalindrom = (input) => {
-    let res = '';
-    for(let i = input.length - 1; i >= 0; i--){
-      res += input[i];
-    }
-    return res == input;
-  };
+  let res = '';
+  for(let i = input.length - 1; i >= 0; i--){
+    res += input[i];
+  }
+  return res == input;
+};
 
-  const howManyPalindromes = (s) => {
-    let len = 0;
-    if(s == ''){
-      return 2685;
-    }
-    for(let i = 0; i < s.length; i++){
-      for(let j = i + 1; j <= s.length; j++){
-        len += checkPalindrom(s.slice(i, j)) ? 1 : 0;
+const howManyPalindromes = (s) => {
+  let len = s.length;
+  if(s.replace(new RegExp(s[0], 'g'), '').length == 0){
+    return (len * (len + 1)) * 0.5;
+  }
+  let r = null;
+  for(let i = 0; i <= s.length * 0.5; i++){
+    for(let j = i + 2; j <= s.length; j++){
+      if(checkPalindrom(s.slice(i, j))){
+        r = new RegExp(s.slice(i, j), 'g');
+        len += s.match(r).length;
       }
     }
-    return len;
   }
+  return len;
+}
 
 // end block
 
